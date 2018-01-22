88e5e40b5b19d6f70fa085c26aa4cf7a1a10a472
Andrew Cameron <andy.d.cameron@gmail.com>
2018-01-22 11:43:10 +0000

    create_table script working

--- scripts/config.py
+++ scripts/config.py
@@ -1,18 +1,15 @@
 ddb = {
-    'tableName': 'testTable',
-    'tableSchema': {
-        'TableName': 'testTable',
-        'AttributeDefinitions': [
-            {'AttributeName': 'test_id', 'AttributeType': 'S'},
-            {'AttributeName': 'test_date', 'AttributeType': 'N'},
-        ],
-        'KeySchema': [
-            {'AttributeName': 'test_id', 'KeyType': 'HASH'},
-            {'AttributeName': 'test_date', 'KeyType': 'SORT'},
-        ],
-        'ProvisionedThroughput': {
-            'ReadCapacityUnits': 5,
-            'WriteCapacityUnits': 5,
-        }
-    },
+    'TableName': 'testTable',
+    'AttributeDefinitions': [
+        {'AttributeName': 'test_id', 'AttributeType': 'S'},
+        {'AttributeName': 'test_date', 'AttributeType': 'N'},
+    ],
+    'KeySchema': [
+        {'AttributeName': 'test_id', 'KeyType': 'HASH'},
+        {'AttributeName': 'test_date', 'KeyType': 'RANGE'},
+    ],
+    'ProvisionedThroughput': {
+        'ReadCapacityUnits': 5,
+        'WriteCapacityUnits': 5,
+    }
 }
--- scripts/create_table.py
+++ scripts/create_table.py
@@ -1,12 +1,31 @@
+# stdlib imports
+import logging
 # 3rd party imports
 import boto3
 # local imports
 import config
 
 
-def create_table(table_name):
+logging.basicConfig()
+log = logging.getLogger(__name__)
+
+
+def create_table(ddb_cfg):
     dynamodb = boto3.resource('dynamodb')
     try:
-        dynamodb.create_table(table_name)
-    except:
-        
+        table = dynamodb.create_table(
+            TableName=ddb_cfg['TableName'],
+            KeySchema=ddb_cfg['KeySchema'],
+            AttributeDefinitions=ddb_cfg['AttributeDefinitions'],
+            ProvisionedThroughput=ddb_cfg['ProvisionedThroughput']
+        )
+    except Exception as e:
+        log.fatal(e)
+        raise SystemExit()
+    
+    table.meta.client.get_waiter('table_exists').wait(TableName=ddb_cfg['TableName'])
+    log.info('Table created: {}'.format(ddb_cfg['TableName']))
+
+
+if __name__ == '__main__':
+    create_table(config.ddb)
